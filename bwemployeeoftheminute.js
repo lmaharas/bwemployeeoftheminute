bf651bbd9a050fa9709a50ac67f29750f8727d24
Lynn Maharas <lynn@betaworks.com>
2014-01-06 12:29:39 -0500

    hmm…not sure what's up here

--- bwemployeeoftheminute.js
+++ bwemployeeoftheminute.js
@@ -13,7 +13,24 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
+
+  Meteor.methods({checkTwitter: function (userId) {
+    check(userId, String);
+    this.unblock();
+    try {
+      var result = HTTP.call("GET", "https://api.twitter.com/1.1/users/show.json?screen_name=lynnhait");
+      console.log(result);
+      console.log(userId);
+      return true;
+    } catch (e) {
+      // Got a network error, time-out or HTTP error in the 400 or 500 range.
+      return false;
+    }
+  }});
+
   Meteor.startup(function () {
     // code to run on server at startup
+    Meteor.call("checkTwitter", "lynnhait", function (error, result) { console.log(error + result); });
   });
+
 }
